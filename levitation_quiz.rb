5c2e68c95c631599a3ccc31e6fab9f221c285415
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-18 23:29:41 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,11 +1,10 @@
 
 def levitation_quiz
+  loop do
 	puts "What is the spell that enacts levitation?"
-	answer = gets.chomp!
-	break if answer = "Wingardium Leviosa"
+	answer = gets.chomp
+	break if answer == "Wingardium Leviosa"
 end
 puts "You passed the quiz!"
 end
-
-levitation_quiz
 
